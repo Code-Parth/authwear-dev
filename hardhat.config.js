6624c3dcec0b753ad54ab8e6e3de5e483376b023
Code-Parth <softsparths@gmail.com>
2023-10-01 19:29:47 +0530

    Create Hardhat environment for my project

---
+++ hardhat.config.js
@@ -0,0 +1,24 @@
+require("@nomicfoundation/hardhat-waffle");
+
+const fs = require('fs');
+const privateKey = fs.readFileSync('.secret').toString();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+module.exports = {
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    apothem: {
+      url: `https://rpc.apothem.network`,
+      accounts: [privateKey],
+    },
+    mumbai: {
+      url: `https://rpc-mumbai.maticvigil.com`,
+      accounts: [privateKey],
+    },
+  },
+  solidity: '0.8.9',
+};
